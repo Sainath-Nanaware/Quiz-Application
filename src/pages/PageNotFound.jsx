a0e194fd6d79518522929894adc2d487b588f34b
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-01-23 23:16:04 +0530

    change application title and design page not found page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,15 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function PageNotFound() {
+  const navigate=useNavigate()
   return (
-    <div>
-      <p>Page Not found!!</p>
+    <div className=' h-[100vh] flex  flex-col justify-center items-center'>
+      <div className=' h-[66vh] w-[54vw] flex flex-col gap-2 justify-center items-center border-2 border-solid border-gray-400 shadow-xl rounded-xl'>
+          <img src="https://cdn.drlinkcheck.com/blog/404-not-found.png" alt="page not found" className=' h-[50vh]' />
+        <Button variant="outlined" onClick={()=>{navigate('/')}}>Home</Button>
+      </div>
     </div>
   )
 }
