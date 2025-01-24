d859305b2ef17974ce333d6511b8f2d82bb4aa63
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2025-01-24 23:05:14 +0530

    add styling on result page

--- src/pages/QuizeResult.jsx
+++ src/pages/QuizeResult.jsx
@@ -35,6 +35,8 @@ function QuizeResult() {
     //final result count according correct quation array length(correct quation count)
     let userMarks=correctQuations.length*eachQuationMarks
     // console.log("correct quation array",correctQuations)
+
+    
     
   return (
     <div>
@@ -45,7 +47,7 @@ function QuizeResult() {
 
       <div className='h-[88vh] flex justify-center items-center '>
         <div className='border-2 border-solid border-[#FAA91C]  shadow-xl rounded-xl h-[80vh] w-[40vw] flex  flex-col justify-start items-center gap-[60px] p-[50px]'>
-            <p className='text-[40px] border-b-2 border-solid border-[#FAA91C]'>{quiz.title}</p>
+            <p className='text-[40px] font-semibold border-b-2 border-solid border-[#FAA91C]'>{quiz.title}</p>
             <div className='flex  gap-[24px]'>
                 <div className='flex flex-col justify-center items-center border-2   border-solid border-green-500 w-[15vw] p-[20px] rounded-xl'>
                     <p className='text-[80px] font-semibold'>{userMarks}</p>
